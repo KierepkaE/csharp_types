e936d8a728ef7a5ed09c82458c5036a41c8af231
kierepkae <kierepkaedyta@gmail.com>
2019-11-15 09:51:34 +0000

    copy value type in another varible copy its value not reference to it

--- Program.cs
+++ Program.cs
@@ -3,6 +3,9 @@ using System;
 namespace TypesPractice {
   class Program {
     static void Main (string[] args) {
-
+      var a = 10;
+      var i = a;
+      b++;
+      Console.WriteLine (strin.Format ("a:{0}, b:{1}", a, b));
     }
   }
